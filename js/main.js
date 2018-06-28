b5a642d5ed14379749df221298432a61a880900f
Dennisse Moreno C <deni.moreno11@gmail.com>
2018-06-28 01:32:38 -0400

    HTML dos ventanas, CSS e intento de JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,47 @@
+
+document.addEventListener('pantallaDos', function() {
+    let elems = document.querySelectorAll('.sidenav-trigger');
+    var instance = M.Sidenav.getInstance(elems);
+    instance.open();
+});
+
+/*let email = document.getElementById("icon_prefix");
+let password = document.getElementById("icon_password");
+
+if(email === "" && password === ""){
+    alert("Tiene campos vacios");
+}
+
+function validateEmail(email) {
+
+    email.password.addEventListener("click", () =>{
+
+        let caracters = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(email){
+
+        }
+    });
+    return caracters.test(email);
+  };
+
+    
+
+
+
+document.getElementById("icon_prefix").addEventListener("click", () => {
+        let textoEmail = event.target;
+        let valido = document.getElementById("valid");
+        let emailValid = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+        
+        if(emailValid.test(textoEmail.value)){
+            valido.innerText = "valido";
+        }else{
+            valido.innerText = "incorrecto";
+        }
+    }
+    );*/
+
+
+
+
 /* Holiii acá va tu código también */
